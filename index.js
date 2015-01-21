c00feed5d0f37cd75e36e5883a999b1517572ca5
Quentin ADAM <quentin.adam@waxzce.org>
2015-01-21 18:48:06 +0100

    qsdfsqdf
    qsdfqsdf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World ! and this is version 2');
+  res.end('Hello World ! and this is version 3 \n PLOP');
 }).listen(8080, '0.0.0.0');
 
 console.log('Server running at http://127.0.0.1:1337/');
